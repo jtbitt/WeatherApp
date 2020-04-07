9f106f52acda50197c5abad7c7c0a8d13779afaa
Jay Bittner <jtbitt@gmail.com>
2020-04-06 20:31:11 -0500

    Set up search input component

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior="height" 
+    >
+      <Text style={[styles.largeText, styles.textStyle]}>
+        San Francisco
+      </Text>
+      <Text style={[styles.smallText, styles.textStyle]}>
+        Light Cloud
+      </Text>
+      <Text style={[styles.largeText, styles.textStyle]}>
+        24°
+      </Text>
+      <TextInput
+        autoCorrect={false} 
+        placeholder="Search any city" 
+        placeholderTextColor="white" 
+        style={styles.textInput} 
+        clearButtonMode="always"
+      />      
+    </KeyboardAvoidingView>
   );
 }
 
@@ -16,4 +34,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+  textAlign: 'center', 
+  fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto', },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+  },
+  textInput: {
+    backgroundColor: '#666', 
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20, 
+    marginHorizontal: 20, 
+    paddingHorizontal: 10, 
+    alignSelf: 'center',
+  }
 });
